0062732268bb2c71c5ab689d188488a71bc1e115
微凉 <i@nn.ci>
2022-01-28 12:53:05 +0800

    :lock: add lock

--- drivers/base/cache.go
+++ drivers/base/cache.go
@@ -6,23 +6,54 @@ import (
 	"github.com/Xhofe/alist/model"
 	"github.com/Xhofe/alist/utils"
 	log "github.com/sirupsen/logrus"
+	"strings"
+	"sync"
+	"time"
 )
 
-func KeyCache(path string, account *model.Account) string {
+var lockMap sync.Map
+
+func CacheKey(path string, account *model.Account) string {
 	path = utils.ParsePath(path)
 	return fmt.Sprintf("%s%s", account.Name, path)
 }
 
 func SetCache(path string, obj interface{}, account *model.Account) error {
-	return conf.Cache.Set(conf.Ctx, KeyCache(path, account), obj, nil)
+	cacheKey := CacheKey(path, account)
+	err := conf.Cache.Set(conf.Ctx, cacheKey, obj, nil)
+	v, ok := lockMap.Load(cacheKey)
+	if ok {
+		cond := v.(*sync.Cond)
+		cond.Broadcast()
+		lockMap.Delete(cacheKey)
+	}
+	return err
 }
 
 func GetCache(path string, account *model.Account) (interface{}, error) {
-	return conf.Cache.Get(conf.Ctx, KeyCache(path, account))
+	cacheKey := CacheKey(path, account)
+	value, err := conf.Cache.Get(conf.Ctx, cacheKey)
+	if err != nil && strings.Contains(strings.ToLower(err.Error()), "value not found") {
+		v, ok := lockMap.Load(cacheKey)
+		if !ok {
+			cond := sync.NewCond(&sync.Mutex{})
+			lockMap.Store(cacheKey, cond)
+			// 超时释放
+			go time.AfterFunc(time.Second*5, func() {
+				cond.Broadcast()
+			})
+			return nil, err
+		} else {
+			cond := v.(*sync.Cond)
+			cond.Wait()
+			return GetCache(cacheKey, account)
+		}
+	}
+	return value, err
 }
 
 func DeleteCache(path string, account *model.Account) error {
-	err := conf.Cache.Delete(conf.Ctx, KeyCache(path, account))
+	err := conf.Cache.Delete(conf.Ctx, CacheKey(path, account))
 	log.Debugf("delete cache %s: %+v", path, err)
 	return err
 }
